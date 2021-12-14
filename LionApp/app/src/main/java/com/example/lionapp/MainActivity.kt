30d8756a467d5da613b18e026d1736d1fe4ae7b0
rlaxogud0909 <rlaxogud0909@gmail.com>
2021-12-14 13:56:34 +0900

    안드로이드 뷰 바인딩

--- LionApp/app/src/main/java/com/example/lionapp/MainActivity.kt
+++ LionApp/app/src/main/java/com/example/lionapp/MainActivity.kt
@@ -4,14 +4,15 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.lionapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 //        val sendButton = findViewById<Button>(R.id.btnSend) // findViewById 옛날 방식
 
@@ -23,17 +24,17 @@ class MainActivity : AppCompatActivity() {
 //            }
 //        })
 
-        btnSend.setOnClickListener {
-            tvHello.text = etMsg.text.toString()
-            etMsg.setText("")
+        binding.btnSend.setOnClickListener {
+            binding.tvHello.text = binding.etMsg.text.toString()
+            binding.etMsg.setText("")
         }
 
-        btnCall.setOnClickListener {
+        binding.btnCall.setOnClickListener {
             val intent: Intent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1111-2222"))
             startActivity(intent)
         }
 
-        btnHome.setOnClickListener {
+        binding.btnHome.setOnClickListener {
             val intent: Intent = Intent(Intent.ACTION_VIEW,
                 Uri.parse("https://www.kakaocorp.com"))
             startActivity(intent)
