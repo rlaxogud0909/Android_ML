31a051b53345cc347e29ae34d5b5f9b74fb40def
rlaxogud0909 <rlaxogud0909@gmail.com>
2021-12-14 13:20:40 +0900

    라이언과 대화하기

--- LionApp/app/src/main/java/com/example/lionapp/MainActivity.kt
+++ LionApp/app/src/main/java/com/example/lionapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.lionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val sendButton = findViewById<Button>(R.id.btnSend) // findViewById 옛날 방식
+
+//        btnSend.setOnClickListener(object : View.OnClickListener{
+//            override fun onClick(p0: View?) {
+//                val msg = etMsg.text.toString()
+//                tvHello.text = msg
+//                etMsg.setText("")
+//            }
+//        })
+
+        btnSend.setOnClickListener {
+            tvHello.text = etMsg.text.toString()
+            etMsg.setText("")
+        }
+
     }
 }
