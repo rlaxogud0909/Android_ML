8d90a098a48236695f3e4e05786f4172372cdaee
rlaxogud0909 <rlaxogud0909@gmail.com>
2021-12-15 11:13:11 +0900

    안드로이드 레이아웃 배치연습 - PhotoActivity.. 이미지변경 추가

--- LionApp/app/src/main/java/com/example/lionapp/PhotoActivity.kt
+++ LionApp/app/src/main/java/com/example/lionapp/PhotoActivity.kt
@@ -2,10 +2,23 @@ package com.example.lionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lionapp.databinding.ActivityPhotoBinding
 
 class PhotoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPhotoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_photo)
+        binding = ActivityPhotoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnUpload.setOnClickListener {
+            binding.imgPhoto.setImageResource(R.drawable.lion)
+        }
+
+        binding.btnDiscard.setOnClickListener {
+            binding.imgPhoto.setImageResource(R.drawable.aff)
+        }
     }
+
 }
