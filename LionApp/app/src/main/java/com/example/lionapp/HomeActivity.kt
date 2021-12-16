8155c20acf14b68825e88fed178b1d7f341992f4
rlaxogud0909 <rlaxogud0909@gmail.com>
2021-12-16 13:55:45 +0900

    안드로이드 화면전환 - 네비게이션 홈 화면..코드로 버튼 추가 리팩토링

---
+++ LionApp/app/src/main/java/com/example/lionapp/HomeActivity.kt
@@ -0,0 +1,44 @@
+package com.example.lionapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import com.example.lionapp.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    private val buttons = mutableMapOf<String, Class<*>>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        buttons["Lion"] = MainActivity::class.java
+        buttons["Count"] = CountActivity::class.java
+
+
+        val params = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+
+
+        for(key in buttons.keys) {
+            val btn = Button(this)
+
+            btn.text = key
+            btn.layoutParams = params
+            btn.setOnClickListener { startActivity( Intent(this, buttons[key])) }
+            binding.layoutMenu.addView(btn)
+        }
+
+
+//        binding.button10.setOnClickListener {
+//            startActivity( Intent(this, MainActivity::class.java) )
+//        }
+
+    }
+}
