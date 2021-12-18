86d9682e32a46b98490537afef6297047977ad05
rlaxogud0909 <rlaxogud0909@gmail.com>
2021-12-18 22:41:21 +0900

    안드로이드 화면전환 - 값 전달, Bundle

--- LionApp/app/src/main/java/com/example/lionapp/MemberActivity.kt
+++ LionApp/app/src/main/java/com/example/lionapp/MemberActivity.kt
@@ -2,10 +2,18 @@ package com.example.lionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lionapp.databinding.ActivityMemberBinding
 
 class MemberActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMemberBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_member)
+        binding = ActivityMemberBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val extras = intent.extras ?: return
+        val id = extras.getString("id")
+        binding.tvHello.text = id + "님 환영합니다!"
     }
 }
