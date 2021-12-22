81bd636a7af3ec5561e2b76cca244a3b56eb48ab
rlaxogud0909 <rlaxogud0909@gmail.com>
2021-12-22 15:22:54 +0900

    Jetpack - 데이터 바인딩 동작 함수와 리스너 바인딩 추가 - 환전 함수 추가 - 이벤트와 리스너 바인딩 : 바인딩 표현식에서 뷰의 이벤트에 응답하는 함수 호출 - 데이터 바인딩 표현식 참고 : https://developer.android.com/topic/libraries/data-binding/expressions

--- JetpackApp/app/src/main/java/com/example/jetpackapp/ui/main/MainViewModel.kt
+++ JetpackApp/app/src/main/java/com/example/jetpackapp/ui/main/MainViewModel.kt
@@ -4,7 +4,17 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    private val used_to_eu_rate = 0.74f
+    private val usd_to_eu_rate = 0.74f
     var dollarText: MutableLiveData<String> = MutableLiveData()
     var result: MutableLiveData<Float> = MutableLiveData()
+
+    fun convertValue() {
+        dollarText.let {
+            if (!it.value.equals(""))
+                result.value = it.value?.toFloat()?.times(usd_to_eu_rate)
+            else
+                result.value = 0f
+        }
+    }
+
 }
