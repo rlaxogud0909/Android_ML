5f2e2d19e3306ef6e5f36f15e1c8da5c11088691
rlaxogud0909 <rlaxogud0909@gmail.com>
2021-12-22 15:06:23 +0900

    Jetpack - 데이터 바인딩 변수 구성

--- JetpackApp/app/src/main/java/com/example/jetpackapp/ui/main/MainFragment.kt
+++ JetpackApp/app/src/main/java/com/example/jetpackapp/ui/main/MainFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.example.jetpackapp.R
 import com.example.jetpackapp.databinding.MainFragmentBinding
+import com.example.jetpackapp.BR.convertViewModel
 
 class MainFragment : Fragment() {
 
@@ -23,7 +24,10 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // UI를 구성하고 레이아웃의 데이터 바인딩 클래스 인스턴스를 생성 및 반환
         binding =  DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
+        // 바인딩 객체는 프래그먼트가 존재하는 동안만 메모리에 남아 있어야 하므로
+        // 프래그먼트가 소멸될 때 바인딩 객체도 소멸되도록 바인딩 객체의 생명주기 소유자를 현재 프래그먼트로 선언
         binding.setLifecycleOwner(this)
         return binding.root
     }
@@ -31,6 +35,8 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding.setVariable(convertViewModel, viewModel)
     }
 
 }
